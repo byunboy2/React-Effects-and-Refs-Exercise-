7f3801b23f2191739dabf3b1cf7ffac1ccb10c08
byunboy2 <76236947+byunboy2@users.noreply.github.com>
2022-11-29 13:03:21 -0800

    Interface is working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [deck, setDeck] = useState("");
+  const [card, setCard] = useState({ data: null, isLoading: true });
+  console.log(card.data)
+  useEffect(function fetchDeckOfCard() {
+    async function fetchDeck() {
+      const deckResult = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+      setDeck(deckResult.data.deck_id);
+    }
+    fetchDeck();
+  }, []);
+
+  useEffect(function fetchNewCard() {
+    async function fetchCard() {
+      const drawnCard = await axios.get(`https://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`);
+      setCard({data: drawnCard.data.cards[0], isLoading: false});
+    }
+    fetchCard();
+  }, [card]);
+
+  function drawCard() {
+    setCard({ data: card, isLoading: true });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={drawCard}>
+        Draw a Card!
+      </button>
+      {card.data===undefined ? <p>No More Cards!!!</p> : <img src={`${card.data.image}`} />}
     </div>
   );
+
 }
 
 export default App;
